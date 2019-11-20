8812caddf4c35560a7b2000f46e7473dc179c9e7
Dischain1 <dischain123321@gmail.com>
2019-11-20 19:53:02 +0700

    Odd views and conroll methods deleted

--- Picsurfer/Controllers/PicturesController.cs
+++ Picsurfer/Controllers/PicturesController.cs
@@ -46,15 +46,11 @@ namespace Picsurfer.Controllers
             return View(picture);
         }
 
-        // GET: Pictures/Create
-        public ActionResult Create()
+        public ActionResult Upload()
         {
             return View();
         }
 
-        // POST: Pictures/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult UploadFiles(HttpPostedFileBase[] filesToUpload)
@@ -78,53 +74,6 @@ namespace Picsurfer.Controllers
             }
         }
 
-        // GET: Pictures/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Picture picture = db.Pictures.Find(id);
-            if (picture == null)
-            {
-                return HttpNotFound();
-            }
-            return View(picture);
-        }
-
-        // POST: Pictures/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Path,Name,Extension")] Picture picture)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(picture).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(picture);
-        }
-
-        // GET: Pictures/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Picture picture = db.Pictures.Find(id);
-            if (picture == null)
-            {
-                return HttpNotFound();
-            }
-            return View(picture);
-        }
-
-        // POST: Pictures/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
